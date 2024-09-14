350fb9c887490d3d078be86063d74239208ef19d
Obliviate <vyacheslavovna.an@mail.ru>
2024-09-14 23:20:07 +0500

    feat: start portfolio page

---
+++ pages/Portfolio.vue
@@ -0,0 +1,15 @@
+<template>
+    <NuxtLayout>
+
+    </NuxtLayout>
+</template>
+
+<script setup>
+import { NuxtLayout } from '#build/components';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
